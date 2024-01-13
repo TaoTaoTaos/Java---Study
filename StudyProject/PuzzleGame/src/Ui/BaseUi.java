6c6dddafaed232b7b76054bf1212da9df5eb0a92
jintao Li <51856377+TaoTaoTaos@users.noreply.github.com>
2024-01-13 15:50:02 +0800

    main

--- StudyProject/PuzzleGame/src/Ui/BaseUi.java
+++ StudyProject/PuzzleGame/src/Ui/BaseUi.java
@@ -2,33 +2,56 @@ package Ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+public class BaseUi extends JFrame implements MouseListener {
 
-public class BaseUi extends JFrame implements KeyListener {
+    Puzzle[][] All_puzzles = new Puzzle[3][3];
+
+    //一个 3*3 的二维数组， 存放所有拼图
+    //数组里全是 Puzzle
 
     public BaseUi() {
 
         initFace();//初始化界面
-        initImage();//初始化图片
+        initPuzzle();//初始化拼图
         JMenuBar();//基础界面的菜单栏
 
 
         this.setVisible(true);//可见
     }
 
-    private void initImage() {
-        int num = 1;
+    private void initPuzzle() {
+
+        //初始化 9 张拼图 ，其中第 9 张 是空白拼图
+        int num = 0;//拼图编号
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                ImageIcon image = new ImageIcon("C:\\Users\\Lijintao\\Desktop\\Java - Study\\StudyProject\\PuzzleGame\\Pictrue\\400x400\\" + (num++) + ".jpg");
-                JLabel Label = new JLabel(image);//创建一个JLabel 用来管理图片
-                Label.setBounds(1 + (134 * j), 134 * i, 134, 134);
-                this.add(Label);
+
+                ImageIcon image = new ImageIcon("C:\\Users\\Lijintao\\Desktop\\Java - Study\\StudyProject\\PuzzleGame\\Pictrue\\400x400\\" + (++num) + ".jpg");
+
+                JLabel label = new JLabel(image);
+
+                All_puzzles[i][j] = new Puzzle(num, i + 1, j + 1, image, label);
+
+                All_puzzles[i][j].getLabel().setBounds(1 + (134 * j), 134 * i, 134, 134);
+
+                if (num == 9) {
+                    All_puzzles[i][j].setSign(1); //其中第 9 张 标识为 “1” 代表特殊的空白拼图
+                } else {
+                    All_puzzles[i][j].setSign(0);
+                }
+
+                this.add(All_puzzles[i][j].getLabel());
+
+                All_puzzles[i][j].show(); //生成 puzzles 时 ，puzzle 【i，j】会展示一次自己的信息
             }
         }
+
+
     }
+
 
     private void JMenuBar() {
         JMenuBar jMenuBar = new JMenuBar();//大菜单
@@ -38,19 +61,21 @@ public class BaseUi extends JFrame implements KeyListener {
         JMenu jMenu2 = new JMenu("关于我");//第二个选项
 
 
-        JMenuItem replay_1 = new JMenuItem("重新开始");//选项 里的 条目1
-
+        JMenuItem random_1 = new JMenuItem("打乱拼图");//选项 里的 条目1
         JMenuItem nextP_2 = new JMenuItem("更换图片");    //条目2
+        JMenuItem Restart_3 = new JMenuItem("重新开始");    //条目3
 
 
-        jMenu1.add(replay_1);//把 条目1 加到第一个选项
+        jMenu1.add(random_1);//把 条目1 加到第一个选项
         jMenu1.add(nextP_2);//把 条目2 加到第一个选项
+        jMenu1.add(Restart_3);//把 条目3 加到第一个选项
 
         jMenuBar.add(jMenu1);//把 第一个选项 加到 大菜单
         jMenuBar.add(jMenu2);//把 第二个选项 加到 大菜单
 
         this.setJMenuBar(jMenuBar);//把上面的东西 设置给 this
     }
+
 
     private void initFace() {
 
@@ -69,23 +94,33 @@ public class BaseUi extends JFrame implements KeyListener {
         this.setLocationRelativeTo(null);//居中
         this.setResizable(false);//不可变大小
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//设置关闭模式
-        this.addKeyListener(this);
+
 
     }
 
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void mouseClicked(MouseEvent e) {
 
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void mousePressed(MouseEvent e) {
 
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
--- StudyProject/PuzzleGame/src/Ui/Puzzle.java
+++ StudyProject/PuzzleGame/src/Ui/Puzzle.java
@@ -1,43 +1,139 @@
 package Ui;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Puzzle {
-    private int number;//拼图编号
-    private Dimension location;//拼图坐标
-    private ImageIcon image;//拼图图片
 
-    public Puzzle(int number, Dimension location, ImageIcon image) {
+public class Puzzle extends JFrame implements MouseListener {
+    private final int number;//拼图编号
+    private int sign;//拼图标识 ： 0 代表普通拼图 ， 1 代表空白的那块特殊拼图
+    private int x_location;//拼图 x 坐标
+    private int y_location;//拼图 y 坐标
+    private final ImageIcon image;//拼图图片
+
+    private JLabel label;
+
+
+    public Puzzle(int number, int x_location, int y_location, ImageIcon image, JLabel label) {
         this.number = number;
-        this.location = location;
+        this.x_location = x_location;
+        this.y_location = y_location;
         this.image = image;
+        this.label = label;
+        this.label.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(x_location + " ， " + y_location + "被点击");
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
     }
 
+    public int getSign() {
+        return sign;
+    }
+
+    public void setSign(int sign) {
+        this.sign = sign;
+    }
 
     public int getNumber() {
         return number;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public int getX_location() {
+        return x_location;
     }
 
-    public Dimension getLocation() {
-        return location;
+    public void setX_location(int x_location) {
+        this.x_location = x_location;
     }
 
-    public void setLocation(Dimension location) {
-        this.location = location;
+    public int getY_location() {
+        return y_location;
     }
+
+    public void setY_location(int y_location) {
+        this.y_location = y_location;
+    }
+
 
     public ImageIcon getImage() {
         return image;
     }
 
-    public void setImage(ImageIcon image) {
-        this.image = image;
+
+    public JLabel getLabel() {
+        return label;
     }
 
+    public void setLabel(JLabel label) {
+        this.label = label;
+    }
 
+    public void show() {
+        System.out.println("===============================================");
+        System.out.println("拼图编号：" + this.getNumber());
+        System.out.println("拼图标识：" + this.getSign());
+        System.out.println("拼图x坐标：" + this.getX_location());
+        System.out.println("拼图y坐标：" + this.getY_location());
+        System.out.println("（" + this.getX_location() + " ， " + this.getY_location() + "）");
+        System.out.println("拼图图片：" + this.getImage());
+        System.out.println("拼图容器：" + this.getLabel());
+        System.out.println("===============================================");
+    }
+
+    public boolean IsClose(Puzzle puzzle1, Puzzle puzzle2) {
+        if (puzzle1.getX_location() - puzzle2.getX_location() == 1 && puzzle1.getY_location() - puzzle2.getY_location() == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
